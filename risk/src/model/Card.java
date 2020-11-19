cb06195fe242f41248d788322e082edd046cbf84
GhazaalTaghavi <62201018+GhazaalTaghavi@users.noreply.github.com>
2020-11-19 10:09:39 -0800

    completing cards model

--- risk/src/model/Card.java
+++ risk/src/model/Card.java
@@ -1,7 +1,39 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Card {
+    private Country country;
+    private Color color;
+    private CardDesigns cardDesigns;
+    private  Player player;
 
+    private Card(Country country, Color color, CardDesigns cardsDesigns) {
+        this.country = country;
+        this.color = color;
+        this.cardDesigns = cardsDesigns;
+        this.player = null;
+    }
 
+    public Country getCountry() {
+        return country;
+    }
 
+    public Color getColor() {
+        return color;
+    }
+
+    public CardDesigns getCardsDesigns() {
+        return cardDesigns;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public static void makeAllCards(ArrayList<Color> colors) {}
 }
---
+++ risk/src/model/CardDesigns.java
@@ -0,0 +1,7 @@
+package model;
+
+public enum CardDesigns {
+    INFANTRY,
+    CAVALRY,
+    ARTILLERY,
+}
