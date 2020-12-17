bcc72c245c142b1d0820fe472ee57dcc604de3d7
amin qodousian <aminghodosian@gmail.com>
2020-12-17 20:22:43 +0330

    FriendsMenu fek konam tamoome

--- risk/src/Main.java
+++ risk/src/Main.java
@@ -1,10 +1,13 @@
+import model.Player;
 import view.MainMenu;
 
 public class Main {
 
     public static void main(String[] args) {
+     //   Player player = new Player("amin","gh","amin","123","a@g.com","09876543212");
         MainMenu mainMenu = new MainMenu(null);
         mainMenu.show();
         mainMenu.execute();
+
     }
 }
