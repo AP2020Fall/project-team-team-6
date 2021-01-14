9ff695de0112e53bcbd8b12c170d74e3dd612d16
aming <aminghodosian@gmail.com>
2021-01-14 20:05:18 +0330

    controllers

--- risk/src/view/Controllers/EditInformationController.java
+++ risk/src/view/Controllers/EditInformationController.java
@@ -1,4 +1,17 @@
 package view.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class EditInformationController {
+    @FXML
+    TextField user;
+    @FXML
+    public void submit(ActionEvent event) {
+    }
+    @FXML
+    public void back(ActionEvent event) {
+    }
 }
