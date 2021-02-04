54caa39d4597b01d37cf3b0cea08d1991cfb46f2
aming <aminghodosian@gmail.com>
2021-02-04 18:01:32 +0330

    controllers

--- risk/src/view/Controllers/ManualPlacementController.java
+++ risk/src/view/Controllers/ManualPlacementController.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -14,7 +15,6 @@ import java.io.IOException;
 import java.net.URL;
 
 public class ManualPlacementController {
-    // TODO: 1/30/2021 ..........baghie kara
 
     @FXML
     private RadioButton mpoff;
@@ -34,6 +34,16 @@ public class ManualPlacementController {
     @FXML
     private RadioButton blizzardon;
 
+    @FXML
+    private ToggleGroup blizzard;
+
+    @FXML
+    private ToggleGroup manual;
+
+    @FXML
+    private ToggleGroup fog;
+
+
 
     public void back(ActionEvent event) throws IOException {
         URL url = new File("risk\\src\\view\\graphic\\RiskGameInformation.fxml").toURI().toURL();
@@ -44,7 +54,13 @@ public class ManualPlacementController {
         window.show();
     }
 
-    public void create(ActionEvent event) throws IOException {
+    public void next(ActionEvent event) throws IOException {
+        RadioButton selectedRadioButtonBlizzard = (RadioButton) blizzard.getSelectedToggle();
+        String blizzardgroup = selectedRadioButtonBlizzard.getText();
+        RadioButton selectedRadioButtonManual = (RadioButton) manual.getSelectedToggle();
+        String manualgroup = selectedRadioButtonBlizzard.getText();
+        RadioButton selectedRadioButtonFof = (RadioButton) fog.getSelectedToggle();
+        String foggroup = selectedRadioButtonBlizzard.getText();
         URL url = new File("risk\\src\\view\\graphic\\MapGames.fxml").toURI().toURL();
         Parent register = FXMLLoader.load(url);
         Scene message = new Scene(register);
