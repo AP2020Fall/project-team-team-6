9ef284443b7aabf295fbe25d19168484dcc3058c
ASUS ZENBOOK <shayanshabanzadeh0@gmail.com>
2021-02-06 21:57:54 +0330

    attacked completed

--- risk/src/view/Controllers/VictoryHintController.java
+++ risk/src/view/Controllers/VictoryHintController.java
@@ -1,18 +1,31 @@
 package view.Controllers;
 
 import javafx.collections.transformation.TransformationList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ResourceBundle;
 
-public class VictoryHintController {
+public class VictoryHintController implements Initializable {
+    @FXML
+    private  ImageView image;
+    private static Image winnerImage;
+
+    public static void setWinnerImage(Image winnerImage) {
+        VictoryHintController.winnerImage = winnerImage;
+    }
+
     public void home(MouseEvent mouseEvent) throws IOException {
         URL url = new File("risk\\src\\view\\graphic\\Mainplato.fxml").toURI().toURL();
         Parent register = FXMLLoader.load(url);
@@ -21,5 +34,10 @@ public class VictoryHintController {
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(message);
         window.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        image.setImage(winnerImage);
     }
 }
